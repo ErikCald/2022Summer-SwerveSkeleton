243fc5ebf05494e67c888bf59ac4986261e32d26
ErikCald <eeejjj8@gmail.com>
2022-06-02 19:04:30 -0400

    Added Comments
    Added a bunch of comments to guide where to add code to SwerveModule.

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -9,10 +9,15 @@ import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
 
+    // CODE: Prepare 2 variables for both SparkMaxs, use the object called CANSparkMax
+
     /**
      * Constructs a SwerveModule.
      */
-    public SwerveModule() {
+    public SwerveModule(/** Add parameters here for CONSTANTS that are specific to each module */) {
+
+        // CODE: Construct both CANSparkMax objects and set all the nessecary settings (get CONSTANTS from Config or from the parameters of the constructor)
+
     }
 
     /**
@@ -33,37 +38,57 @@ public class SwerveModule {
         // Optimize the reference state to avoid spinning further than 90 degrees
         Rotation2d measuredAngle = getSteeringAngle();
         SwerveModuleState state = SwerveModuleState.optimize(desiredState, measuredAngle);
-        Rotation2d angle = ContinousPIDSparkMax.calculate(state.angle, measuredAngle);
         double velocity = state.speedMetersPerSecond;
+        Rotation2d angle = ContinousPIDSparkMax.calculate(state.angle, measuredAngle);
         
-        // Set the PositionConversionFactor ahead of time and then pass radians to position PID on steering SparkMax.
+        
+        // CODE: Pass the velocity (which is in meters per second) to velocity PID on drive SparkMax. (VelocityConversionFactor set so SparkMax wants m/s)
 
-        // Set the VelocityConversionFactor ahead of time and then pass m/s to velocity PID on drive SparkMax.
+        // CODE: Pass the angle (which is in radians) to position PID on steering SparkMax. (PositionConversionFactor set so SparkMax wants radians)
+
     }
 
+    /**
+     * Returns the velocity of the wheel in meters per second.
+     * 
+     * @return meters per second of the wheel
+     */
     public double getVelocity() {
 
-        // Read encoder velocity from drive SparkMax, set the VelocityConversionFactor ahead of time so it's in unit of m/s.
+        // CODE: Read encoder velocity from drive SparkMax and return m/s. (VelocityConversionFactor set so SparkMax returns m/s))
 
         return 0.0;
     }
 
+    /**
+     * Returns the angle the wheel is pointing in a Rotation2d.
+     * 
+     * @return angle of the wheel
+     */
     public Rotation2d getSteeringAngle() {
 
-        // Read encoder position from steering SparkMax, set the PositionConversionFactor ahead of time so it's in unit of radians.
+        // CODE: Read encoder position from steering SparkMax. (PositionConversionFactor set so SparkMax returns radians)
 
         return new Rotation2d(0);
     }
 
+    /**
+     * Gets a reading from the Lamprey and updates the SparkMax encoder (interal NEO encoder).
+     * This is specific to Swerge. Other methods need to be written for other hardware.
+     */
     public void updateSteeringFromLamprey() {
         
-        // Read value from Lamprey and set internal Neo encoder for the steering SparkMax (but need to add an offset first)
+        // CODE: You can attempt this if you want but this will probably be done together in the 2nd or 3rd meeting.
+        // CODE: Read value from Lamprey and set internal Neo encoder for the steering SparkMax (but need to add an offset first)
 
     }
 
+    /**
+     * Standard stop motors method for every subsystem.
+     */
     public void stopMotors() {
 
-        // Call the stopMotors method (provided with all WPILib motor controller objects)
+        // CODE: Call the stopMotors method in the CANSparkMax (provided with all WPILib motor controller objects)
 
     }
 
